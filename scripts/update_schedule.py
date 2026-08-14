71e29e95a748dce12c9fd1dc0a3be69c818cce2b
Leonhart322 <lambhart322414@gmail.com>
2026-08-15 08:16:15 +0900

    Update update_schedule.py

--- scripts/update_schedule.py
+++ scripts/update_schedule.py
@@ -439,8 +439,8 @@ def get_softbank_starter(url):
         "name": match.group(2),
         "pitches": int(match.group(3)),
         "innings": match.group(5),
-        "runs": int(match.group(12)),
-        "earnedRuns": int(match.group(13)),
+        "runs": int(match.group(13)),
+        "earnedRuns": int(match.group(14)),
         "decision": decision
     }
 
